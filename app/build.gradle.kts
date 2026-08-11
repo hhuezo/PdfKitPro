91d2d2cd678ccf7a93952de2222aa43414782ff1
hhuezo <hhuezo@ista.gob.sv>
2026-08-11 14:57:09 -0600

    Actualiza la versión de la aplicación a 1.1.3 y el versionCode a 14.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hhuezo.pdfconverter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.1.2"
+        versionCode = 14
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
